8cf4d85322489fb78ad243e439d2231cfc5e4861
Cody Adcock <codyadcock10@gmail.com>
2018-09-06 10:47:38 -0600

    Initial Commit

---
+++ whyiOSFailableInit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whyiOSFailableInit
+//
+//  Created by Cody on 9/6/18.
+//  Copyright © 2018 Cody Adcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
